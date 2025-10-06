2077fbb14d6a245e0c1f7fd165445f3d6a2d5ebb
demuelle <demuelle@gmail.com>
2025-10-06 16:42:25 -0500

    fixes message on BadNicknameException

--- src/main/java/com/demuelle/fake_football/exception/BadNicknameException.java
+++ src/main/java/com/demuelle/fake_football/exception/BadNicknameException.java
@@ -2,6 +2,6 @@ package com.demuelle.fake_football.exception;
 
 public class BadNicknameException extends RuntimeException {
     public BadNicknameException(String nickname) {
-        super("There is no team called \"nickname\"");
+        super("There is no team called \"" + nickname + "\"");
     }
 }
